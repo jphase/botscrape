be9b55393a5f4fec4d514cb5ec8a0ddace2d3faa
Jeff Hays <jeff@offthewallmedia.com>
2013-10-22 03:16:06 -0700

    added bot settings object. added filter function to fix relative links by adding domain to begining of urls... will be brushed up later

--- api/controllers/SandboxController.js
+++ api/controllers/SandboxController.js
@@ -11,7 +11,14 @@ module.exports = {
 		// Initialize
 		var cheerio = require('cheerio');
 		var request = require('request');
-		var matched = [];
+		var extend = require('jquery-extend');
+
+		// Settings
+		var bot = {
+			matched: [],
+			domain: false,
+			links: true
+		}
 
 		// Escape all RegEx reserved characters from string - http://stackoverflow.com/questions/3446170/escape-string-for-use-in-javascript-regex#6969486
 		function escRegExp(str) {
@@ -19,37 +26,47 @@ module.exports = {
 		}
 
 		// Return content based on match values in database for matched values
-		function matchLine($, valz) {
+		function matchLine($, valz, optz) {
 			// Create a regex based on the bot match string
 			var regex = new RegExp(escRegExp(valz.match_string), 'gim');
 			// Generate results based on regex matches within match_parent
 			var results = [];
-			// Check for martch_parent and match_el
+			// Check for match_parent and match_el and loop through each
 			if($(valz.match_parent).length && $(valz.match_el).length) {
 				$(valz.match_el).each(function(index, element) {
-					// For using .toString() method on regex match
-					var item = element;
-					// Match regex on entire parent
-					// var match = item.toString().match(regex);
+					// Match regex on current match_el
 					var match = $(this).text().match(regex);
 					if(match != 'undefined' && match != null) {
 						// Change match to content from return_el
 						$(this).find(valz.return_el).each(function(index, child_el) {
-							sails.log.debug($(this));
-							results.push($(this).text());
+							// Push matches to results array
+							// sails.log.debug($(this).html());
+							results.push(filter($(this), optz));
 						});
-						if(match.length) results.push(match);
+						results.push('<br>');
+						// if(match.length) results.push(match);
 					}
 				});
 			}
 			return results;
 		}
 
+		// Filter matched elements
+		function filter($, optz) {
+			// Filter links
+			if(optz.links) {
+				$.find('a').each(function(index, a) {
+					// Add bot URL to relative links
+					if(!this.attr('href').indexOf(optz.domain) >= 0) {						
+						this.attr('href', optz.domain + this.attr('href'));
+					}
+				});
+			}
+			return $;
+		}
+
 		// Render view after data is retrieved
 		var successCallback = function(bot, data) {
-			// Debug
-			// sails.log.debug(data);
-			// Push data to view, kthxbye
 			res.view({
 				bot: bot,
 				results: data.toString()
@@ -64,7 +81,7 @@ module.exports = {
 			Sandbox.find()
 			.where({ id: req.param('id') })
 			.done(function(err, sandbox) {
-				// Throw teh errorz
+				// Throw errors
 				if(err) { throw err; }
 				// Publish
 				Sandbox.publish(req.socket, sandbox);
@@ -73,11 +90,12 @@ module.exports = {
 					if(!error && response.statusCode == 200) {
 						// Set $ as cheerio html DOM object from request URL
 						var $ = cheerio.load(html);
-						// Return entire line when match is found
-						var matched = matchLine($, sandbox[0]);
-						successCallback(sandbox[0], matched);
-						// Debug
-						// sails.log.debug(matched);
+						// Set bot domain as url without trailing slash
+						bot.domain = sandbox[0].url.replace(/\/$/, '');
+						// Set bot matched content
+						bot.matched = matchLine($, sandbox[0], bot);
+						// Callback
+						successCallback(sandbox[0], bot.matched);
 					}
 				});
 			});
